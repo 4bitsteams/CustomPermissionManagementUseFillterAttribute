0dea9b29729c44043cf948cf9b3c2fd513ec130b
md.rubel <md.rubel@HOAP2406101764A>
2021-09-02 12:37:40 +0600

    Add project files.

---
+++ CustomPermissionManagementUseFillterAttribute/ActionFillter/AbcAuthorizeAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPermissionManagementUseFillterAttribute.Models
+{
+    public class AbcAuthorizeAttribute : ActionFilterAttribute
+    {
+        private String _right;
+        public AbcAuthorizeAttribute(String _right)
+        {
+            this._right = _right;
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //var hasClaim = filterContext.HttpContext.User.Claims.Where(c => c.Type == StaticDefinedData.Rights);
+
+            //List<string> rightList = new List<string>();
+            //foreach (var item in hasClaim)
+            //{
+            //    rightList.Add(item.Value.ToString());
+            //}
+            ////var principal = this.HttpContext.User;
+            ////if (principal == null) throw new Exception("User not found");
+            ////var claims = principal.Claims.ToList();
+
+            if (_right != "CAN_VIEW_USER_LIST1")
+            {
+                filterContext.Result = new RedirectResult("~/");
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ CustomPermissionManagementUseFillterAttribute/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using CustomPermissionManagementUseFillterAttribute.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPermissionManagementUseFillterAttribute.Controllers
+{
+    public class TestController : Controller
+    {
+        [AbcAuthorizeAttribute("CAN_VIEW_USER_LIST")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
